fc6b676528518862c9feab24890e5263e1e95e95
aysunabizada <nabizada.aysu@email.com>
2026-04-15 16:16:14 +0400

    UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { FaRegEdit } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 
-
 function App() {
     const [columns, setColumns] = useState({
         todo: {
@@ -39,15 +38,16 @@ function App() {
         const task = {
             id: crypto.randomUUID(),
             content: newTask
-        };
+        }
 
         setColumns(prev => ({
             ...prev,
-            todo: {
-                ...prev.todo,
-                items: [...prev.todo.items, task]
+            [activeColumns]: {  
+                ...prev[activeColumns],
+                items: [...prev[activeColumns].items, task]
             }
-        }));
+        }))
+
         setNewTask("");
     }
 
@@ -101,89 +101,101 @@ function App() {
         setDraggedItem(null);
     }
 
-    console.log(columns);
-
     return (
-        <main className="min-h-screen w-full flex items-center justify-center flex-col gap-10">
-            <h1 className="text-4xl font-extrabold text-[#303650] text-center mt-8">Drag and Drop Kanban Board</h1>
-            <div className="rounded-lg bg-amber-50 max-w-lg xl:w-full flex overflow-hidden shadow mx-3">
-                <input className="focus:outline-none w-full py-3 text-gray-800 inp placeholder:text-gray-500 pl-5" placeholder="Add something..."
-                    type="text"
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                    onKeyDown={(e) => e.key === "Enter" && addNewTask()}
-                />
+        <main className="main">
+            <div className="container">
+                <h1 className="title">Drag and Drop Kanban Board</h1>
+                <div className="input-bar shadow">
+                    <input placeholder="Add smth..."
+                        type="text"
+                        id=""
+                        name=""
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
+                        onKeyDown={(e) => e.key === "Enter" && addNewTask()}
+                    />
 
-                <select value={activeColumns} name="" id=""
-                    className="px-2 text-black mr-2 outline-none border-l cursor-pointer"
-                    onChange={(e) => setActiveColumns(e.target.value)}
-                >
-                    {Object.keys(columns).map((columnId) => (
-                        <option value={columnId} key={columnId}>
-                            {columns[columnId].name}
-                        </option>
-                    ))}
-                </select>
-
-                <button onClick={addNewTask} className="text-white cursor-pointer bg-amber-500 px-8 font-bold">Add</button>
-            </div>
-            <div className="flex items-center justify-center gap-8 flex-wrap overflow-x-auto w-full mb-8 text-white">
-                {Object.keys(columns).map((columnId) => (
-                    <div
-                        key={columnId}
-                        className='w-80 h-96 bg-[#30364F] rounded-xl p-5 overflow-auto [&::-webkit-scrollbar]:w-px'
-                        onDragOver={(e) => handleDragOver(e, columnId)}
-                        onDrop={(e) => handleDrop(e, columnId)}
+                    <select
+                        value={activeColumns}
+                        name=""
+                        id=""
+                        onChange={(e) => setActiveColumns(e.target.value)}
                     >
-                        <div className="bg-amber-50 rounded-xl flex items-center justify-between text-black px-4 py-2 mb-3">
-                            <p className="font-medium">{columns[columnId].name}</p>
-                            <span className="bg-amber-500 text-white w-6 h-6 flex justify-center items-center rounded-full">
-                                {columns[columnId].items.length}
-                            </span>
+                        {Object.keys(columns).map((columnId) => (
+                            <option
+                                value={columnId}
+                                key={columnId}>
+                                {columns[columnId].name}
+                            </option>
+                        ))}
+                    </select>
+
+                    <button onClick={addNewTask} >Add</button>
+                </div>
+                <div className="board">
+                    {Object.keys(columns).map((columnId) => (
+                        <div
+                            key={columnId}
+                            className='column'
+                            onDragOver={(e) => handleDragOver(e, columnId)}
+                            onDrop={(e) => handleDrop(e, columnId)}
+                        >
+                            <div className="column-header">
+                                <p>{columns[columnId].name}</p>
+                                <span className="badge">
+                                    {columns[columnId].items.length}
+                                </span>
+                            </div>
+                            <div className="tasks">
+                                {
+                                    columns[columnId].items.length === 0 ? (
+                                        <p className="message">Add or drop tasks here</p>
+                                    ) :
+                                        (columns[columnId].items.map((item) => {
+                                            const isEditing = editingId === item.id;
+                                            return (
+                                                <div
+                                                    key={item.id}
+                                                    draggable={!isEditing}
+                                                    onDoubleClick={() => handleEdit(item)}
+                                                    onDragStart={() => handleOnDrag(columnId, item)}
+                                                    className="task-card"
+                                                >
+                                                    {isEditing ? (
+                                                        <input
+                                                            value={text}
+                                                            onChange={(e) => setText(e.target.value)}
+                                                            onBlur={() => saveEdit(columnId, item.id)}
+                                                            onKeyDown={(e) => {
+                                                                if (e.key === "Enter") saveEdit(columnId, item.id);
+                                                            }}
+                                                            autoFocus
+                                                            id=""
+                                                            name=""
+                                                            className="edit-input"
+                                                        />
+                                                    ) : (
+                                                        <span className="task-text">{item.content}</span>
+                                                    )}
+                                                    {!isEditing && (
+                                                        <div className="task-icons">
+                                                            <FaRegEdit
+                                                                onClick={() => handleEdit(item)}
+                                                                className="edit icon"
+                                                            />
+                                                            <FaXmark
+                                                                onClick={() => removeTask(columnId, item.id)}
+                                                                className="delete icon"
+                                                            />
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            );
+                                        }))}
+                            </div>
                         </div>
-                        <div className="flex flex-col gap-2">
-                            {columns[columnId].items.map((item) => {
-                                const isEditing = editingId === item.id;
-                                return (
-                                    <div
-                                        key={item.id}
-                                        draggable={!isEditing}
-                                        onDoubleClick={() => handleEdit(item)}
-                                        onDragStart={() => handleOnDrag(columnId, item)}
-                                        className="rounded-xl cursor-pointer flex items-center border border-[#222636] hover:border-[#fe9900] justify-between bg-[#222636] px-4 py-2.5"
-                                    >
-                                        {isEditing ? (
-                                            <input
-                                                value={text}
-                                                onChange={(e) => setText(e.target.value)}
-                                                onBlur={() => saveEdit(columnId, item.id)}
-                                                onKeyDown={(e) => {
-                                                    if (e.key === "Enter") saveEdit(columnId, item.id);
-                                                }}
-                                                autoFocus
-                                                className="bg-transparent outline-none text-white w-full"
-                                            />
-                                        ) : (
-                                            <span className="truncate">{item.content}</span>
-                                        )}
-                                        {!isEditing && (
-                                            <div className="flex gap-2 items-center">
-                                                <FaRegEdit
-                                                    onClick={() => handleEdit(item)}
-                                                    className="cursor-pointer hover:text-blue-600"
-                                                />
-                                                <FaXmark
-                                                    onClick={() => removeTask(columnId, item.id)}
-                                                    className="cursor-pointer hover:text-red-600"
-                                                />
-                                            </div>
-                                        )}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </main>
     )
